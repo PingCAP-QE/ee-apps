784efe39e11a35e1e4fbeaed877659dbb8ada9f8
wuhuizuo <wuhuizuo@126.com>
2024-11-07 12:31:51 +0000

    feat(publisher): pretty the notify lark message
    Signed-off-by: wuhuizuo <wuhuizuo@126.com>

--- publisher/go.mod
+++ publisher/go.mod
@@ -3,6 +3,7 @@ module github.com/PingCAP-QE/ee-apps/publisher
 go 1.23
 
 require (
+	github.com/Masterminds/sprig/v3 v3.3.0
 	github.com/PingCAP-QE/ee-apps/dl v0.0.0-20241104081507-4ebce35328e3
 	github.com/cloudevents/sdk-go/v2 v2.15.2
 	github.com/go-redis/redis/v8 v8.11.5
@@ -17,6 +18,9 @@ require (
 )
 
 require (
+	dario.cat/mergo v1.0.1 // indirect
+	github.com/Masterminds/goutils v1.1.1 // indirect
+	github.com/Masterminds/semver/v3 v3.3.0 // indirect
 	github.com/aws/smithy-go v1.22.0 // indirect
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
@@ -26,23 +30,29 @@ require (
 	github.com/go-chi/chi/v5 v5.1.0 // indirect
 	github.com/go-logr/logr v1.4.2 // indirect
 	github.com/gorilla/websocket v1.5.3 // indirect
+	github.com/huandu/xstrings v1.5.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/compress v1.17.9 // indirect
 	github.com/ks3sdklib/aws-sdk-go v1.3.0
 	github.com/manveru/faker v0.0.0-20171103152722-9fbc68a78c4d // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mitchellh/copystructure v1.2.0 // indirect
+	github.com/mitchellh/reflectwalk v1.0.2 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/opencontainers/go-digest v1.0.0 // indirect
 	github.com/opencontainers/image-spec v1.1.0
 	github.com/pierrec/lz4/v4 v4.1.21 // indirect
 	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
+	github.com/shopspring/decimal v1.4.0 // indirect
+	github.com/spf13/cast v1.7.0 // indirect
 	github.com/stretchr/testify v1.9.0 // indirect
 	go.opentelemetry.io/otel v1.31.0 // indirect
 	go.opentelemetry.io/otel/trace v1.31.0 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
 	go.uber.org/zap v1.27.0 // indirect
+	golang.org/x/crypto v0.28.0 // indirect
 	golang.org/x/mod v0.21.0 // indirect
 	golang.org/x/net v0.30.0 // indirect
 	golang.org/x/sync v0.8.0 // indirect
---
+++ publisher/pkg/impl/tiup_worker_test.go
@@ -0,0 +1,144 @@
+package impl
+
+import (
+	"errors"
+	"testing"
+	"time"
+
+	"github.com/go-redis/redis/v8"
+	"github.com/rs/zerolog"
+)
+
+func Test_tiupWorker_notifyLark(t *testing.T) {
+	t.Skipf("It is manually test case")
+	const testWebhookURL = "https://open.feishu.cn/open-apis/bot/v2/hook/<please-replace-it>"
+
+	type fields struct {
+		logger      zerolog.Logger
+		redisClient redis.Cmdable
+		options     struct {
+			LarkWebhookURL   string
+			MirrorURL        string
+			PublicServiceURL string
+			NightlyInterval  time.Duration
+		}
+	}
+	type args struct {
+		req *PublishRequest
+		err error
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+	}{
+		{
+			name: "Empty webhook URL",
+			fields: fields{
+				logger: zerolog.New(nil),
+				options: struct {
+					LarkWebhookURL   string
+					MirrorURL        string
+					PublicServiceURL string
+					NightlyInterval  time.Duration
+				}{
+					LarkWebhookURL: "",
+				},
+			},
+			args: args{
+				req: &PublishRequest{
+					Publish: PublishInfo{
+						Name:    "test-package",
+						Version: "v1.0.0",
+					},
+				},
+				err: errors.New("test error"),
+			},
+		},
+		{
+			name: "Valid notification with all fields",
+			fields: fields{
+				logger: zerolog.New(nil),
+				options: struct {
+					LarkWebhookURL   string
+					MirrorURL        string
+					PublicServiceURL string
+					NightlyInterval  time.Duration
+				}{
+					LarkWebhookURL:   testWebhookURL,
+					MirrorURL:        "https://test-mirror.com",
+					PublicServiceURL: "https://test-service.com",
+				},
+			},
+			args: args{
+				req: &PublishRequest{
+					From: From{
+						Type: FromTypeOci,
+						Oci: &FromOci{
+							Repo: "test-repo",
+							Tag:  "test-tag",
+						},
+					},
+					Publish: PublishInfo{
+						Name:    "test-package",
+						Version: "v1.0.0",
+						OS:      "linux",
+						Arch:    "amd64",
+					},
+				},
+				err: errors.New("publish failed"),
+			},
+		},
+		{
+			name: "Invalid webhook URL",
+			fields: fields{
+				logger: zerolog.New(nil),
+				options: struct {
+					LarkWebhookURL   string
+					MirrorURL        string
+					PublicServiceURL string
+					NightlyInterval  time.Duration
+				}{
+					LarkWebhookURL: "invalid-url",
+				},
+			},
+			args: args{
+				req: &PublishRequest{
+					Publish: PublishInfo{
+						Name:    "test-package",
+						Version: "v1.0.0",
+					},
+				},
+				err: errors.New("test error"),
+			},
+		},
+		{
+			name: "Missing publish info",
+			fields: fields{
+				logger: zerolog.New(nil),
+				options: struct {
+					LarkWebhookURL   string
+					MirrorURL        string
+					PublicServiceURL string
+					NightlyInterval  time.Duration
+				}{
+					LarkWebhookURL: testWebhookURL,
+				},
+			},
+			args: args{
+				req: &PublishRequest{},
+				err: errors.New("missing publish info"),
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			p := &tiupWorker{
+				logger:      tt.fields.logger,
+				redisClient: tt.fields.redisClient,
+				options:     tt.fields.options,
+			}
+			p.notifyLark(tt.args.req, tt.args.err)
+		})
+	}
+}
