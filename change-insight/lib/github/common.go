6a24c3d937d04193e7a6e0e734ffd8866c9fe6ba
wuhuizuo <wuhuizuo@126.com>
2024-11-16 15:47:39 +0800

    Update common.go

--- change-insight/lib/github/common.go
+++ change-insight/lib/github/common.go
@@ -27,31 +27,20 @@ type labelInfo struct {
 	Description string `json:"description"`
 }
 
-type Config struct {
-	Token  string
-	ApiURL string
+func getGitHubToken() string
+	return os.Getenv("GITHUB_TOKEN")
 }
 
-func NewConfig() (*Config, error) {
-	token := os.Getenv("GITHUB_TOKEN")
-	if token == "" {
-		return nil, fmt.Errorf("GITHUB_TOKEN environment variable is not set")
-	}
-	return &Config{
-		Token:  token,
-		ApiURL: "https://api.github.com/repos/",
-	}, nil
-}
-
-func apiCall(config *Config, apiUrl string) (*http.Response, error) {
+func apiCall(apiUrl string) (*http.Response, error) {
 	req, err := http.NewRequest("GET", apiUrl, nil)
 	if err != nil {
 		log.Printf("request init error: %s\n", err.Error())
 		return nil, err
 	}
+	
 	req.Header.Set("Accept", "application/vnd.github+json")
-	req.Header.Set("Authorization", "token "+config.Token)
-
+	req.Header.Set("Authorization", "token " + getGitHubToken())
+	
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
 		log.Printf("request error: %s\n", err.Error())
