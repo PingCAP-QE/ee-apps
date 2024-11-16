44ca89a747f1dd088a42c7360fe1e18bb8c70b5b
wuhuizuo <wuhuizuo@126.com>
2024-11-16 15:42:11 +0800

    Update common.go
    load GitHub token from env var.

--- change-insight/lib/github/common.go
+++ change-insight/lib/github/common.go
@@ -3,12 +3,14 @@ package github
 import (
 	"log"
 	"net/http"
+	"os"
 )
 
 type Repo struct {
 	Org  string // eg: pingcap
 	Repo string // eg: tidb
 }
+
 type UserInfo struct {
 	Login     string `json:"login"`
 	Id        int    `json:"id"`
@@ -25,17 +27,41 @@ type labelInfo struct {
 	Description string `json:"description"`
 }
 
-var token string = "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"
-var apiURL string = "https://api.github.com/repos/"
+type Config struct {
+	Token  string
+	ApiURL string
+}
 
-func apiCall(apiUrl string) (*http.Response, error) {
+func NewConfig() (*Config, error) {
+	token := os.Getenv("GITHUB_TOKEN")
+	if token == "" {
+		return nil, fmt.Errorf("GITHUB_TOKEN environment variable is not set")
+	}
+	return &Config{
+		Token:  token,
+		ApiURL: "https://api.github.com/repos/",
+	}, nil
+}
+
+func apiCall(config *Config, apiUrl string) (*http.Response, error) {
 	req, err := http.NewRequest("GET", apiUrl, nil)
 	if err != nil {
-		log.Printf("request init error : %s \n", err.Error())
+		log.Printf("request init error: %s\n", err.Error())
 		return nil, err
 	}
 	req.Header.Set("Accept", "application/vnd.github+json")
-	req.Header.Set("Authorization", "token "+token)
-	//log.Printf("req: %+v \n", req)
-	return http.DefaultClient.Do(req)
+	req.Header.Set("Authorization", "token "+config.Token)
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		log.Printf("request error: %s\n", err.Error())
+		return nil, err
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		log.Printf("unexpected status code: %d\n", resp.StatusCode)
+		return resp, fmt.Errorf("unexpected status code: %d", resp.StatusCode)
+	}
+
+	return resp, nil
 }
