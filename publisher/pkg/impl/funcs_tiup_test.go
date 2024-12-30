069f4f2136768acd68d2e168971caccfd8e3169b
wuhuizuo <wuhuizuo@126.com>
2024-12-30 16:57:21 +0800

    fix(publisher): fix ga oci judgement (#218)
    Signed-off-by: wuhuizuo <wuhuizuo@126.com>
    
    Signed-off-by: wuhuizuo <wuhuizuo@126.com>

---
+++ publisher/pkg/impl/funcs_tiup_test.go
@@ -0,0 +1,31 @@
+package impl
+
+import "testing"
+
+func Test_transformTiupVer(t *testing.T) {
+	type args struct {
+		version string
+		tag     string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "test1",
+			args: args{
+				version: "v8.5.0-pre",
+				tag:     "v8.5.0-centos7_linux_amd64",
+			},
+			want: "v8.5.0-centos7",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := transformTiupVer(tt.args.version, tt.args.tag); got != tt.want {
+				t.Errorf("transformTiupVer() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
