5569c8c44ae8198a858849dd2cafc45c31e49ed9
wuhuizuo <wuhuizuo@126.com>
2025-06-03 17:30:08 +0800

    feat(insight/cralwlers): move DSN conversion to new utility file

--- insight/crawlers/ci/prow-jobs.ts
+++ insight/crawlers/ci/prow-jobs.ts
@@ -1,6 +1,6 @@
 import { parseArgs } from "jsr:@std/cli@1.0.14/parse-args";
 import * as mysql from "https://deno.land/x/mysql@v2.12.1/mod.ts";
-
+import { convertDsnToClientConfig } from "../../db/utils.ts";
 interface prowJobRun {
   kind: string;
   metadata: {
@@ -151,38 +151,6 @@ export async function saveJob(
   }
 
   await client.execute(insertOrUpdateSql, values);
-}
-
-export function convertDsnToClientConfig(dsn: string) {
-  // mysql://user:password@host:port/database
-  const [_, config] = dsn.split("://");
-  const [credentials, hostAndPort] = config.split("@");
-  if (!credentials || !hostAndPort) {
-    throw new Error("Invalid DSN: missing user/password or host/port");
-  }
-
-  const [username, password] = credentials.split(":");
-  if (!username || !password) {
-    throw new Error("Invalid DSN: missing user or password");
-  }
-
-  const [host, portAndDb] = hostAndPort.split(":");
-  if (!host || !portAndDb) {
-    throw new Error("Invalid DSN: missing host or port/database");
-  }
-
-  const [port, db] = portAndDb.split("/");
-  if (!port || !db) {
-    throw new Error("Invalid DSN: missing port or database");
-  }
-
-  return {
-    hostname: host,
-    port: parseInt(port),
-    username: decodeURIComponent(username),
-    password: decodeURIComponent(password),
-    db,
-  } as mysql.ClientConfig;
 }
 
 async function main() {
