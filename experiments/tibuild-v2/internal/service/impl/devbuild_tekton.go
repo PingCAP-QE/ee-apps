7301d82312d70c0f818294871df26f7859f3758c
wuhuizuo <wuhuizuo@126.com>
2026-04-02 19:16:34 +0800

    feat(tibuild-v2): add platform field to DevBuild model

--- experiments/tibuild-v2/internal/service/impl/devbuild_tekton.go
+++ experiments/tibuild-v2/internal/service/impl/devbuild_tekton.go
@@ -16,32 +16,54 @@ const (
 	ceTypeFakeGHPushDevBuild   = "net.pingcap.tibuild.devbuild.push"
 	ceTypeFakeGHPRDevBuild     = "net.pingcap.tibuild.devbuild.pull_request"
 	ceTypeFakeGHCreateDevBuild = "net.pingcap.tibuild.devbuild.create"
+
+	// Platforms
+	LinuxAmd64  = "linux/amd64"
+	LinuxArm64  = "linux/arm64"
+	DarwinAmd64 = "darwin/amd64"
+	DarwinArm64 = "darwin/arm64"
 )
 
 func (s *devbuildsrvc) triggerTknBuild(ctx context.Context, record *ent.DevBuild) (*ent.DevBuild, error) {
 	// 1. Compose a cloud event from the record.
-	event, err := newDevBuildCloudEvent(record)
+	events, err := newDevBuildCloudEvents(record)
 	if err != nil {
-		s.logger.Err(err).Msg("failed to create cloud event")
+		s.logger.Err(err).Msg("failed to create cloud events")
 		return nil, err
 	}
 
-	// 2. Send the cloud event to tekton listener that serves for tibuild.
-	respEvent, result := s.tektonCloudEventClient.Request(ctx, *event)
-	if !protocol.IsACK(result) {
-		s.logger.Err(result).Msg("failed to send cloud event")
-		return nil, fmt.Errorf("failed to send cloud event: %w", result)
-	}
+	// 2. Send the cloud events to tekton listener that serves for tibuild.
+	eventsCount := len(events)
+	for i, event := range events {
+		respEvent, result := s.tektonCloudEventClient.Request(ctx, *event)
+		if !protocol.IsACK(result) {
+			s.logger.Err(result).Msgf("failed to send cloud event(index %d/%d", i, eventsCount)
+			return nil, fmt.Errorf("failed to send cloud event: %w", result)
+		}
 
-	// debug the resp event
-	if respEvent != nil {
-		s.logger.Debug().Stringer("response_event", respEvent).Msg("event sent ok with response")
+		// debug the resp event
+		if respEvent != nil {
+			s.logger.Debug().Stringer("response_event", respEvent).Msgf("event(index %d/%d) sent ok with response", i, eventsCount)
+		}
 	}
 
 	return record, nil
 }
 
-func newDevBuildCloudEvent(record *ent.DevBuild) (*cloudevents.Event, error) {
+func newDevBuildCloudEvents(record *ent.DevBuild) ([]*cloudevents.Event, error) {
+	events := []*cloudevents.Event{}
+	for _, platform := range parsePlatforms(record.Platform) {
+		event, err := newDevBuildCloudEvent(record, platform)
+		if err != nil {
+			return nil, err
+		}
+		events = append(events, event)
+	}
+
+	return events, nil
+}
+
+func newDevBuildCloudEvent(record *ent.DevBuild, platform string) (*cloudevents.Event, error) {
 	event := cloudevents.NewEvent()
 
 	var eventData any
@@ -72,12 +94,32 @@ func newDevBuildCloudEvent(record *ent.DevBuild) (*cloudevents.Event, error) {
 	event.SetSource("tibuild.pingcap.net/api/devbuilds/" + fmt.Sprint(record.ID))
 	event.SetExtension("user", record.CreatedBy)
 	event.SetExtension("paramProfile", normalizeEdition(string(record.Edition)))
+	event.SetExtension("paramIsHotfix", record.IsHotfix)
+	event.SetExtension("paramIsPushGcr", record.IsPushGcr)
 	if record.GithubRepo != "" {
 		event.SetExtension("paramGithubRepo", record.GithubRepo)
 	}
 	if record.BuilderImg != "" {
 		event.SetExtension("paramBuilderImage", record.BuilderImg)
 	}
+	if platform != "" {
+		event.SetExtension("paramPlatform", platform)
+	}
 
 	return &event, nil
+}
+
+func parsePlatforms(platformExp string) []string {
+	switch platformExp {
+	case LinuxAmd64, LinuxArm64, DarwinAmd64, DarwinArm64:
+		return []string{platformExp}
+	case "linux", "linux/all":
+		return []string{LinuxAmd64, LinuxArm64}
+	case "darwin", "darwin/all":
+		return []string{DarwinAmd64, DarwinArm64}
+	case "", "all":
+		return []string{""}
+	default:
+		return strings.Split(platformExp, ",")
+	}
 }
